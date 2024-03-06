252f826db7900e97e2a175c9e4f8decb75b85ebb
LeanNaikim <leangnaikim168@gmail.com>
2024-03-06 15:34:48 +0700

    update new

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,30 @@ import CardFeedbackComponent from "./components/CardFeedbackComponent";
 function App() {
   return (
     <>
-      <dev className="h-screen flex flex-col justify-between">
+      <div className="h-screen flex flex-col justify-between">
         <NavbarComponent/>
         <main className="m-10">
           <h1 className="m-5 text-4xl font-semibold text-center">Product</h1>
-          <dev className="flex flex-row justify-between flex-wrap gap-y-4">
+          <div className="flex flex-row justify-between flex-wrap gap-y-4">
             <CardProductComponent/>
             <CardProductComponent/>
             <CardProductComponent/>
             <CardProductComponent/>
             <CardProductComponent/>
             <CardProductComponent/>
-          </dev>
+          </div>
 
     
 
           <h1 className="m-5 text-4xl font-semibold text-center">Feedback</h1>
-          <dev className="flex flex-row justify-between flex-wrap gap-y-4">
+          <div className="flex flex-row justify-between flex-wrap gap-y-4">
             <CardFeedbackComponent/>
             <CardFeedbackComponent/>
             <CardFeedbackComponent/>  
-          </dev>
+          </div>
         </main>
         <FooterComponent/>
-      </dev>
+      </div>
     </>
   );
 }
